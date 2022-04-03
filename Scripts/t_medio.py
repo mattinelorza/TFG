e79beb437bd6e54f80d39e7e8a2b182074df84d5
mattinelorza <60148919+mattinelorza@users.noreply.github.com>
2022-04-03 20:14:53 +0200

    T_medio actualizado

--- Scripts/t_medio.py
+++ Scripts/t_medio.py
@@ -4,18 +4,18 @@
 
 
 from sys import argv
-import pandas as pd
 import numpy as np
 
 arr = np.loadtxt(argv[1], dtype="int")
 
-print(arr)
-
-data = pd.DataFrame(arr)
-res = pd.DataFrame(data.values[::2]-data.values[1::2])
-res2= res.astype(int)
-print(res2)
-
-np.savetxt("resultados.csv",res2.astype(int),fmt='%i')
 
 
+data = np.array(arr)
+res = np.array(data[::2]-data[1::2])
+
+
+
+np.savetxt("resultados.csv",res,fmt='%i')
+
+
+print("Tiempo medio: %.2f ms" % (np.mean(res)/1000))
